debb2a9c075c887343e81a1b64ade3c12fe6c50c
andycheng <andychz0519@gmail.com>
2020-04-07 19:28:57 +0800

    Initial Commit

---
+++ 00657025_hw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00657025_hw2
+//
+//  Created by User12 on 2020/4/7.
+//  Copyright © 2020 User12. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
